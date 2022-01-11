cc0b4763a7ffee31f1169569d618a0050e0f002c
gerads <pdgerads@gmail.com>
2022-01-11 17:42:03 -0600

    Create inventory-plus.js
    Special version with fixed weight calculation.

---
+++ inventory-plus.js
@@ -0,0 +1,17 @@
+
+{
+	"name": "inventory-plus",
+	"title": "Inventory+",
+	"description": "Enhances the default dnd5e sheets inventory to allow for reorganization",
+	"version": "0.3.6",
+	"author": "Felix#6196",
+	"systems": ["dnd5e"],
+	"esmodules": ["./inventory-plus.js"],
+	"styles": ["inventory-plus.css"],
+	"packs": [],
+    "url": "https://github.com/ZeroXNoxus/inventory-plus",
+    "manifest": "https://raw.githubusercontent.com/ZeroXNoxus/inventory-plus/master/module.json",
+    "download": "https://github.com/ZeroXNoxus/inventory-plus/archive/refs/heads/master.zip",
+    "minimumCoreVersion": "0.8.8",
+    "compatibleCoreVersion": "0.8.8"
+}
